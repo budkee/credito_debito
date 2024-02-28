9f4581622a59a5fa4566a0f5aaa7751196637392
Isabelle  Bicudo <87657255+IsabelleBic20@users.noreply.github.com>
2024-02-27 20:18:15 -0400

    Second commit

--- shardA.py
+++ shardA.py
@@ -1,25 +1,36 @@
+# shard_a.py
 import socket
 import json
+import queue
 
-def credito(data_operacao, conta_cliente, valor_operacao):
-    resposta_data = {
-        "status": "OK"
-    }
-    return json.dumps(resposta_data)
+class ShardA:
+    def __init__(self):
+        self.queue = queue.Queue()
+        self.saldo = 5000
+
+    def credito(self,data_operacao, conta_cliente, valor_operacao):
+        
+        self.saldo+=valor_operacao 
+        valores_credito=self.saldo
+
+        resposta_data = {   
+            "status": "OK",
+            "novo_valor_credito": valores_credito
+        }
+
+        self.queue.put(resposta_data)
+        return json.dumps(resposta_data)#retorna valor atualizado da conta
 
 def shard_a():
-    # configurando shard A
+    # lendo ip do coordenator
     shard_a_host = '192.168.0.41'  
     shard_a_port = 8888  
 
-    #criando socket TCP para o shard A
+    shard_a_instancia = ShardA()
+
     shard_a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ver isso aqui!
-
-    # vinculando socket com host e porta do server
-    shard_a_socket.bind((shard_a_host, shard_a_port))
-
-    #conexões recebidas
-    shard_a_socket.listen(5)
+    shard_a_socket.connect((shard_a_host, shard_a_port))
+    #shard_a_socket.listen(5)
 
     print(f"[*] Shard A comunicando-se em {shard_a_host}:{shard_a_port}")
 
@@ -28,29 +39,29 @@ def shard_a():
         coordenador_socket, addr = shard_a_socket.accept()
         print(f"[*] Conexão aceita {addr[0]}:{addr[1]}")
 
-        ###
         solicitacao = coordenador_socket.recv(1024).decode('utf-8')
 
         try:
             data = json.loads(solicitacao)
-            if "operacao" in data and data["operacao"] == "Credito":
+            if "operacao" in data and data["operacao"] == "C":
         
                 data_operacao = data["data"]
                 conta_cliente = data["conta_cliente"]
                 valor_operacao = data["value"]
 
                 # enviando resposta para coordenador
-                resposta = credito(data_operacao, conta_cliente, valor_operacao)
+                resposta =shard_a_instancia.credito(data_operacao, conta_cliente, valor_operacao)
                 coordenador_socket.send(resposta.encode('utf-8'))
             else:
                 coordenador_socket.send("Operação Inválida".encode('utf-8'))
         
-        except json.JSONDecodeError:
-            # Erro cod JSON
-            coordenador_socket.send("Possível erro de comunicação com Server")
-
+        except json.JSONDecodeError as e:
+            # Erro de decodificação JSON
+            coordenador_socket.send(f"Possível erro de comunicação com Server: {str(e)}".encode('utf-8'))
         
-        coordenador_socket.close()
+        finally:
+            coordenador_socket.close()
+
 
 if __name__ == "__main__":
     shard_a()
